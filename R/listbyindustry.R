ec0730a83e34c78f48773195b53860ea30cffb0d
fcluzeau <francois.cluzeau@gmail.com>
2016-07-12 18:12:27 +0200

    Update listbyindustry.R

--- R/listbyindustry.R
+++ R/listbyindustry.R
@@ -1,6 +1,6 @@
 listbyindustry <- function(){
   newtable <- with(stocktable, data.frame(id=paste0("symbol_", Ticker), text=paste(Ticker, "-", Company), leaf=TRUE, Industry=Industry, Sector=Sector));
-  mydata <- splitIntoTree(newtable, "Sector");
+  mydata <- splitIntoTree(newtable, "Country");
   mydata$children <- lapply(mydata$children, splitIntoTree, f="Industry", out=c("id", "text", "leaf"));
   invisible(list("text"=".", children=mydata))
 }
